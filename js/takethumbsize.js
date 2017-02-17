b8fa416d0275c94453349010e64e48be68450f6a
Pondok Teknologi <faazabilamr084@gmail.com>
2017-02-18 03:55:24 +0700

    Add files via upload

---
+++ js/takethumbsize.js
@@ -0,0 +1,3 @@
+<script type='text/javascript'>
+document.write(pondokteknologi(&quot;<data:post.thumbnailUrl/>&quot;,&quot;<data:post.title/>&quot;));
+</script>
---
+++ js/thumbsize.js
@@ -0,0 +1,6 @@
+<script type='text/javascript'>
+//<![CDATA[
+function pondokteknologi(e,t){var n=200;var r=150;image_tag='<img width="'+n+'" height="'+r+'" src="'+e.replace("/s72-c/","/w"+n+"-h"+r+"-c/")+'" alt="'+t.replace(/"/g,"")+'" title="'+t.replace(/"/g,"")+'" class="post-thumbnail"/>';if(t!="")return image_tag;else return""}
+//]]>
+</script>
+
