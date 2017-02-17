c0a79e2ce7085257c09c9790e15fe82047948a6d
Pondok Teknologi <faazabilamr084@gmail.com>
2017-02-18 04:34:03 +0700

    Update index.php

--- product/19 video tutorial/index.php
+++ product/19 video tutorial/index.php
@@ -1,2 +1,4 @@
-header("Location: http://idx8.xyz/r/1096/41016/113/");
-?>
+< html>< head>
+< script>
+< !– window.location = “http://idx8.xyz/r/1096/41016/113/” //–>
+< /script>< /head>< body>< /body>< /html>< /p>
